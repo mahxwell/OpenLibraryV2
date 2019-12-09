867f8a8b1c34becf7dcb184308e3cfd5bad8f99a
mahxwell <maxime.soro@gmail.com>
2019-12-09 23:10:54 +0100

    Travis Integration

--- openlibclient/openlibclient-client/src/main/java/org/mahxwell/openlib/service/copy/Copy_Service.java
+++ openlibclient/openlibclient-client/src/main/java/org/mahxwell/openlib/service/copy/Copy_Service.java
@@ -30,7 +30,7 @@ public class Copy_Service
         URL url = null;
         WebServiceException e = null;
         try {
-            url = new URL("http://library_web_service:8080/Openlibrary-Webservice/copy?wsdl");
+            url = new URL("http://localhost:8080/Openlibrary-Webservice/copy?wsdl");
         } catch (MalformedURLException ex) {
             e = new WebServiceException(ex);
         }
